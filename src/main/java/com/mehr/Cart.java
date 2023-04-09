98fb62e09f8ff369fd6360620832810c84dc6beb
Mehrunissa Pirwani (2303.KHI.CND.007) <127718818+MehrXloop@users.noreply.github.com>
2023-04-10 03:55:37 +0500

    AC 3 test passed

--- src/main/java/com/mehr/Cart.java
+++ src/main/java/com/mehr/Cart.java
@@ -6,10 +6,12 @@ import java.util.List;
 public class Cart {
     List<CartItem> items = new LinkedList<>();
     Integer totalPrice = 0;
+    Integer totalQuantity =0;
 
    public void addItem(Item item , Integer quantity){
      CartItem cartItem = new CartItem(item, quantity);
      items.add(cartItem);
      totalPrice += item.price * quantity;
+     totalQuantity += quantity;
    }
 }
