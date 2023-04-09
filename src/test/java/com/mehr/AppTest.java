85a8696688e21363620e6399667c057b1f660f68
Mehrunissa Pirwani (2303.KHI.CND.007) <127718818+MehrXloop@users.noreply.github.com>
2023-04-10 00:51:10 +0500

    AC 1 test failed

--- src/test/java/com/mehr/AppTest.java
+++ src/test/java/com/mehr/AppTest.java
@@ -15,4 +15,12 @@ class AppTest {
     void testApp() {
         assertEquals(1, 1);
     }
+    //AC 1 Given that I a new shopper, when I begin shopping, then I expect my cart to be empty.
+    @Test
+     void emptylist(){
+        //Setup
+        Cart cart = new Cart();
+        //Assert
+        assertEquals(0,cart.items.size());
+     }
 }
