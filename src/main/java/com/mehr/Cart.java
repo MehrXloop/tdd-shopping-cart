ccde4f2f2abdb035138b39a426af7913deb3f920
Mehrunissa Pirwani (2303.KHI.CND.007) <127718818+MehrXloop@users.noreply.github.com>
2023-04-10 05:14:50 +0500

    AC 7 test passed

--- src/main/java/com/mehr/Cart.java
+++ src/main/java/com/mehr/Cart.java
@@ -6,26 +6,35 @@ import java.util.List;
 public class Cart {
     List<CartItem> items = new LinkedList<>();
     Integer totalPrice = 0;
-    Integer totalQuantity =0;
+    Integer totalQuantity = 0;
 
-   public void addItem(Item item , Integer quantity){
-     CartItem cartItem = new CartItem(item, quantity);
-     items.add(cartItem);
-     totalPrice += item.price * quantity;
-     totalQuantity += quantity;
-   }
-   
-   public  List<String> itemizedList(){
-    return items.stream().map(cartItem ->{
-        if(cartItem.item.onSale){
-            return cartItem.item.name + " - x"+cartItem.quantity +" $"+cartItem.item.price +" highlighted";
+    public void addItem(Item item, Integer quantity) {
+        CartItem cartItem = new CartItem(item, quantity);
+        items.add(cartItem);
+        totalPrice += item.price * quantity;
+        totalQuantity += quantity;
+    }
+
+    public List<String> itemizedList() {
+        return items.stream().map(cartItem -> {
+            if (cartItem.item.onSale) {
+                return cartItem.item.name + " - x" + cartItem.quantity + " $" + cartItem.item.price + " highlighted";
+            }
+            return cartItem.item.name + " - x" + cartItem.quantity + " $" + cartItem.item.price;
+        }).toList();
+
+    }
+
+    public void removeItem(String name) {
+        for (CartItem cartItem : items) {
+            if (cartItem.item.name == name) {
+                if (cartItem.quantity > 1) {
+                    cartItem.quantity -= 1;
+                } else {
+                    items.remove(cartItem);
+                }
+            }
         }
-          return cartItem.item.name + " - x"+cartItem.quantity +" $"+cartItem.item.price;
-    }).toList();
+    }
 
-   }
-
-   public void removeItem(String name){
-    items = items.stream().filter(cartItem -> cartItem.item.name != name).toList();
-   }
 }
